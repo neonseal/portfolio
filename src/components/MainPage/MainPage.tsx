c5d16e0d47a04da88ac122b5d5d6de295fc7f086
WhiterDig <zachking96@gmail.com>
2022-10-21 21:01:48 -0400

    Update MainPage

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,11 +1,11 @@
-import "./MainPage.scss";
+import styles from "./MainPage.module.scss";
 
 import React, { FC } from "react";
 
 interface MainPageProps {}
 
 const MainPage: FC<MainPageProps> = () => (
-  <div className="container">MainPage Component</div>
+  <div className={styles.container}>MainPage Component</div>
 );
 
 export default MainPage;
