e1c2968a3fdd5f7f6664d34510937ed6ad2eb565
Corey SIeland-Harris <corey.sielandharris@gmail.com>
2022-10-21 22:18:25 -0400

    Add and configure eslint and prettier

--- src/components/MainPage/MainPage.lazy.tsx
+++ src/components/MainPage/MainPage.lazy.tsx
@@ -1,11 +1,11 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 
 const LazyMainPage = lazy(() => import('./MainPage'));
 
-const MainPage = (props: JSX.IntrinsicAttributes & { children?: React.ReactNode; }) => (
-  <Suspense fallback={null}>
-    <LazyMainPage {...props} />
-  </Suspense>
+const MainPage = (props: JSX.IntrinsicAttributes & { children?: React.ReactNode }) => (
+    <Suspense fallback={null}>
+        <LazyMainPage {...props} />
+    </Suspense>
 );
 
 export default MainPage;
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,11 +1,9 @@
-import styles from "./MainPage.module.scss";
+import React, { FC } from 'react'
 
-import React, { FC } from "react";
+import styles from './MainPage.module.scss'
 
 interface MainPageProps {}
 
-const MainPage: FC<MainPageProps> = () => (
-  <div className={styles.container}>MainPage Component</div>
-);
+const MainPage: FC<MainPageProps> = () => <div className={styles.container}>MainPage Component</div>
 
-export default MainPage;
+export default MainPage
