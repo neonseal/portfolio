db7efaf2a18ee54ab1c4242f7cbf1b92426ce8aa
Corey SIeland-Harris <corey.sielandharris@gmail.com>
2022-10-21 20:42:49 -0400

    Merge branches

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-
-import MainPage from './components/MainPage/MainPage';
 import React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="*" element={<Navigate to="" replace />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
