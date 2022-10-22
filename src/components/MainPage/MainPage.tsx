78c9dbc96c8a0680f8860ec9b2f3dcc9e365e57c
WhiterDig <zachking96@gmail.com>
2022-10-21 22:48:46 -0400

    fix: Fix weird ES issues
    Change ES target version to ES2022, and fix resulting issues

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -2,28 +2,38 @@ import React, { FC } from 'react';
 
 import styles from './MainPage.module.scss';
 
-interface MainPageProps { }
+interface MainPageProps {}
 
-interface HeaderLink {name: string}
+interface HeaderLink {
+    name: string;
+}
 
-const headerLinks: HeaderLink[] = [{name: 'Home'}, {name: 'Skills'}, {name: 'Projects'}, {name: 'Experience'}, {name: 'Contact Me'}]
+const headerLinks: HeaderLink[] = [
+    { name: 'Home' },
+    { name: 'Skills' },
+    { name: 'Projects' },
+    { name: 'Experience' },
+    { name: 'Contact Me' },
+];
 
 const MainPage: FC<MainPageProps> = () => (
-  <div className={styles.container}>
-    <div className={styles.header}>
-      {headerLinks.map((link) => {
-        return (<span className={styles.header_link}>{link.name}</span>)
-      })}
+    <div className={styles.container}>
+        <div className={styles.header}>
+            {headerLinks.map((link) => (
+                <span key={link.name} className={styles.header_link}>
+                    {link.name}
+                </span>
+            ))}
+        </div>
+        <div className={styles.intro}>
+            <img className={styles.intro_image} src="https://via.placeholder.com/314" alt="Interesting" />
+            <div className={styles.intro_text}>
+                <div className={styles.name}>FIRST NAME LAST NAME</div>
+                <div className={styles.description}>FULL-STACK SOFTWARE ENGINEER & SOMETHING ELSE</div>
+                <button>Contact me</button>
+            </div>
+        </div>
     </div>
-    <div className={styles.intro}>
-      <img className={styles.intro_image} src='https://via.placeholder.com/314' alt="Interesting"/>
-      <div className={styles.intro_text}>
-        <div className={styles.name}>FIRST NAME LAST NAME</div>
-        <div className={styles.description}>FULL-STACK SOFTWARE ENGINEER & SOMETHING ELSE</div>
-        <button>Contact me</button>
-      </div>
-    </div>
-  </div>
 );
 
 export default MainPage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import './index.css';
+import React from 'react';
 
 import App from './App';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <React.StrictMode>
+    <StrictMode>
         <App />
-    </React.StrictMode>
+    </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
